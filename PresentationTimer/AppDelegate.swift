ece1712f98ec8c1fc280bff111fb1bf0a922e917
Len Mahaffey <len.mahaffey@gmail.com>
2018-04-03 20:17:14 -0600

    Initial Commit

---
+++ PresentationTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PresentationTimer
+//
+//  Created by Len Mahaffey on 4/3/18.
+//  Copyright © 2018 TLA Designs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
