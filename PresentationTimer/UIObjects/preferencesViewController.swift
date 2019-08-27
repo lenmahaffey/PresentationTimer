e0c83eaed541778c5aa333c024495237d0de7230
Len Mahaffey <len.mahaffey@gmail.com>
2019-08-26 20:20:06 -0600

    removed text boxes on background color selection since values can be entered into the NSColorWell.  Also set the backgroundColorPicker to be set to black to match the background

--- PresentationTimer/UIObjects/preferencesViewController.swift
+++ PresentationTimer/UIObjects/preferencesViewController.swift
@@ -8,20 +8,17 @@
 
 import Cocoa
 
-var backgroundColor = NSColor.brown.cgColor
+var backgroundColor = NSColor.black.cgColor
 class preferencesViewController: NSViewController {
     
     let nc = NotificationCenter.default
     @IBOutlet weak var backgroundColorPicker: NSColorWell!
-    @IBOutlet weak var backgroundRGBRedTextField: NSTextField!
-    @IBOutlet weak var backgroundRGBGreenTextField: NSTextField! 
-    @IBOutlet weak var backgroundRGBBlueTextField: NSTextField!
-    @IBOutlet weak var backgroundRGBAlphaTextField: NSTextField!
     @IBOutlet weak var fontColorPicker: NSColorWell!
     @IBOutlet weak var fontSelector: NSPopUpButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        backgroundColorPicker.color = NSColor.black
         getFonts()
         // Do view setup here.
     }
@@ -36,39 +33,8 @@ class preferencesViewController: NSViewController {
         }
     }
     
-    func colorFromTextField() {
-        let red = CGFloat(Float(self.backgroundRGBRedTextField.intValue) / Float(255))
-        let green = CGFloat(Float(self.backgroundRGBGreenTextField.intValue) / Float(255))
-        let blue = CGFloat(Float(self.backgroundRGBBlueTextField.intValue) / Float(255))
-        let alpha = CGFloat(1)
-        let newColor = NSColor(cgColor: CGColor(red: red, green: green, blue: blue, alpha: alpha))
-        self.backgroundColorPicker.color = newColor!
-        backgroundColor = newColor!.cgColor
-        nc.post(name: Notification.Name.setBackgroundColor, object: self)
-    }
-    
     @IBAction func backgroundColorPickerAction(_ sender: Any) {
-        self.backgroundRGBRedTextField.intValue = Int32(Float32(self.backgroundColorPicker.color.redComponent) * 255)
-        self.backgroundRGBGreenTextField.intValue = Int32(Float32(self.backgroundColorPicker.color.greenComponent) * 255)
-        self.backgroundRGBBlueTextField.intValue = Int32(Float32(self.backgroundColorPicker.color.blueComponent) * 255)
         backgroundColor = backgroundColorPicker.color.cgColor
         nc.post(name: Notification.Name.setBackgroundColor, object: self)
     }
-    
-    @IBAction func backgroundRGBRedTextFieldAction(_ sender: Any) {
-        colorFromTextField()
-    }
-    
-    @IBAction func backgroundRGBGreenTextFieldAction(_ sender: Any) {
-        colorFromTextField()
-    }
-    
-    @IBAction func backgroundRGBBlueTextFieldAction(_ sender: Any) {
-        colorFromTextField()
-    }
-    
-    @IBAction func backgroundRGBAlphaTextFieldAction(_ sender: Any) {
-        colorFromTextField()
-    }
-    
 }
